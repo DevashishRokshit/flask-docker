39897f56e5871bede7c4f47b1be8f510a5d9b4a6
Devashish <dev.rokshit@gmail.com>
2025-06-04 20:58:26 +0530

    added codeploy.sh

---
+++ scripts/install_codedeploy_agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum update -y
+yum install ruby wget -y
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+systemctl enable codedeploy-agent
+systemctl start codedeploy-agent
+
