aaec1cb92f03977b81fe04d6055ec19a1ba57c41
Devashish <dev.rokshit@gmail.com>
2025-06-04 17:37:03 +0530

    updated new

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Flask App Deployed via Docker and GitHub Actions!!!"
+    return """
+    ✅ Flask App Deployed via Docker and GitHub Actions!!!
+    🚀 Auto Deployment with CodeDeploy & ASG Successful.
+    📦 Image pulled from Docker Hub.
+    """
+
+@app.route('/health')
+def health_check():
+    return jsonify(status="UP", message="App is running correctly"), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
