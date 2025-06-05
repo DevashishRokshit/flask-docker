82229a60020c24512c8a21402905083127eb562b
Devashish <dev.rokshit@gmail.com>
2025-06-05 10:48:39 +0530

    testing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return (
-        "✅ Flask App Deployed via DockerHub and GitHub Actions!!!<br>"
+        "✅ Flask App Deployed via DockerHub and GitHub Actions!!<br>"
         "🔁 Automatically builds and pushes Docker images on code change.<br>"
         "🚀 Running inside a container served via AWS ALB!!"
     )
